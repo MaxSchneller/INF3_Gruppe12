bcf1595b72e473bd0359326bbd1c41542a7fe7c8
MarkusRein <Markus.Rein@Student.Reutlingen-University.DE>
2015-12-18 08:18:35 +0100

    comments and theoretical tasks

--- csharp/Aufgabenblatt3_Parser/EBNF_Parser/Program.cs
+++ csharp/Aufgabenblatt3_Parser/EBNF_Parser/Program.cs
@@ -11,6 +11,15 @@ namespace EBNF_Parser
 
         }
 
+        //   <ebnf> "Arithmetic Expression" {
+        //       expression = term {("+" | "-") term}.
+        //       term = factor {( "*" | "/" ) factor}.
+        //       factor = constant | variable | "(" expression ")" .
+        //       variable = "x" | "y" | "z" .
+        //       constant = digit {digit} .
+        //       digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
+        //   }</ebnf>
+
         //String is the arithmetic Expression, we work with.
         private string arithmeticExpression;
 
@@ -19,9 +28,11 @@ namespace EBNF_Parser
         {
             this.arithmeticExpression = getArithmeticExpression;
             Console.WriteLine(expression());
-            Console.WriteLine("Die AE ist jetzt: " + arithmeticExpression);
         }
 
+        //Method 1: expression. Is expression TRUE or FALSE?
+        //Is first a term? proof if after term is "+" or "-" then delet
+        //then proof if element after is term,too
         public bool expression()
         {
             bool isExpression = false;
@@ -61,6 +72,11 @@ namespace EBNF_Parser
             return isExpression;
         }
 
+
+        //Method 2: term. Is term TRUE or FALSE?
+        //Called from expression
+        //Is first a factor? proof if after term is "*" or "/" then delet
+        //then proof if element after is factor,too
         public bool term(string isNextTerm)
         {
             bool isTerm = false;
@@ -94,6 +110,9 @@ namespace EBNF_Parser
             return isTerm;
         }
 
+        //Method 3: factor. Is factor TRUE or FALSE?
+        //Called from term
+        //Is first a constant, variable or "(" then proof if next expression, then proof closing ")"
         public bool factor(string isNextFactor)
         {
             bool isFactor = false;
@@ -141,6 +160,9 @@ namespace EBNF_Parser
             return isFactor;
         }
 
+        //Method 4: variable. Is variable TRUE or FALSE?
+        //Called from factor
+        //Is first a "x" or "y" or "z" then delet
         public bool variable(string isNextVariable)
         {
             bool isVariable = false;
@@ -152,6 +174,9 @@ namespace EBNF_Parser
             return isVariable;
         }
 
+        //Method 5: constant. Is constant TRUE or FALSE?
+        //Called from factor
+        //Is first a digit. If true delete then proof is after then next is true
         public bool constant(string isNextConstant)
         {
             bool isConstant = false;
@@ -174,6 +199,9 @@ namespace EBNF_Parser
             return isConstant;
         }
 
+        //Method 6: digit. Is digit TRUE or FALSE?
+        //Called from constant
+        //Is the element a number between 0 and 9
         public bool digit(string isThisDigit)
         {
             return (isThisDigit.Equals("0") || isThisDigit.Equals("1") || isThisDigit.Equals("2") || isThisDigit.Equals("3") ||
